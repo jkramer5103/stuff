8d2aee58c1b2b15076c4233827851593dea58e64
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-17 11:01:40 +0200

    Update build.py

--- build.py
+++ build.py
@@ -2,45 +2,42 @@ import secrets
 from os import system
 import pyperclip
 import re
-print("1. Build")
-print("2. Upload zu PyPi")
-onetwo = str(input("Was willst du machen (1,2)? "))
-if onetwo == "1":
-    system("del dist\\* /F /Q")
-    filename = "setup.py"
-    # Öffne die setup.py Datei und lies ihren Inhalt
-    with open(filename, "r") as file:
-        setup_content = file.read()
 
-    # Finde die aktuelle Version
-    version_pattern = r"version\s*=\s*'(\d+\.\d+)'"
-    match = re.search(version_pattern, setup_content)
+system("del dist\\* /F /Q")
+filename = "setup.py"
+# Öffne die setup.py Datei und lies ihren Inhalt
+with open(filename, "r") as file:
+	setup_content = file.read()
 
-    if match:
-        current_version = match.group(1)
-        major, minor = current_version.split(".")
-        new_version = f"{major}.{int(minor) + 1}"
+# Finde die aktuelle Version
+version_pattern = r"version\s*=\s*'(\d+\.\d+)'"
+match = re.search(version_pattern, setup_content)
 
-        # Ersetze die alte Version mit der neuen Version
-        new_content = re.sub(version_pattern, f"version='{new_version}'", setup_content)
+if match:
+	current_version = match.group(1)
+	major, minor = current_version.split(".")
+	new_version = f"{major}.{int(minor) + 1}"
 
-        # Schreibe den neuen Inhalt zurück in die Datei
-        with open(filename, "w") as file:
-            file.write(new_content)
+	# Ersetze die alte Version mit der neuen Version
+	new_content = re.sub(version_pattern, f"version='{new_version}'", setup_content)
 
-        print(f"Neue Version: {new_version}")
-    else:
-        print("Konnte keine Version in der setup.py finden.")
-        exit()
+	# Schreibe den neuen Inhalt zurück in die Datei
+	with open(filename, "w") as file:
+		file.write(new_content)
 
-
-
-    system("python setup.py sdist bdist_wheel")
-elif onetwo == "2":
-    
-    print("Drücke Strg + V und danach Enter")
-    pyperclip.copy(secrets.apikey)
-    system("twine upload dist/*")
+	print(f"Neue Version: {new_version}")
 else:
-    print("Keine gültige Auswahloption")
-    exit()
+	print("Konnte keine Version in der setup.py finden.")
+	exit()
+
+
+
+system("python setup.py sdist bdist_wheel")
+
+
+print("Drücke Strg + V und danach Enter")
+pyperclip.copy(secrets.apikey)
+system("twine upload dist/*")
+
+print("Keine gültige Auswahloption")
+exit()
